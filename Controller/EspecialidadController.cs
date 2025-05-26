a64683a6295bc3e101cdd388d0b5430bb2909301
Andres Casallas <carloscasallas137@gmail.com>
2025-05-25 23:08:58 -0500

    Creacion del controller de especialidades y exposicion de servicios en sawagger

---
+++ Controller/EspecialidadController.cs
@@ -0,0 +1,28 @@
+﻿using MediReserva.Models;
+using MediReserva.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediReserva.Controllers  
+{
+    [ApiController]
+    [Route("api/[controller]")]   
+
+    public class EspecialidadController : ControllerBase
+    {
+        private readonly IEspecialidadService _especialidadService;
+
+        public EspecialidadController(IEspecialidadService especialidadService)
+        {
+            _especialidadService = especialidadService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<List<Especialidad>>), 200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetAll()
+        {
+            var especialidades = await _especialidadService.GetAllAsync();
+            return Ok(new ApiResponse<List<Especialidad>>(especialidades, true, "La especialidades fueron consultadas con exito"));
+        }
+    }
+}
