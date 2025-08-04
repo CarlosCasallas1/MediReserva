3894849053e764599fb5a08d1250690a734ff3e9
Andres Casallas <carloscasallas137@gmail.com>
2025-08-03 22:41:55 -0500

    Se a creado la interfaz de medico

---
+++ Services/Interfaces/IMedicoService.cs
@@ -0,0 +1,34 @@
+﻿using MediReserva.Models;
+
+namespace MediReserva.Services.Interfaces
+{
+    public interface IMedicoService
+    {
+        //<summary>
+        //retorna todos los medicos incluyendo sus relaciones 
+        //</summary>
+        Task<List<Medico>> GetAllAsync();
+
+        //<summary>
+        //retorna un medico por su id
+        //</summary>
+        Task<Medico?> GetByIdAsync();
+
+        //<summary>
+        //crear un nuevo medico
+        //</summary>
+        Task<Medico?> CreateAsync(Medico medico);
+
+        //<summary>
+        //actualizar medico existente
+        //</summary>
+        Task<bool> UpdateAsync(Medico medico);
+
+        //<summary>
+        //elimina medico por su id
+        //</summary>
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
+
